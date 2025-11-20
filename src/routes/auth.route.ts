df8ad21e7b59eeca3667e2605260471308b89509
ashishkunthe <ashishkunthe2003@gmail.com>
2025-11-20 22:29:58 +0530

    added the auth middleware for the admin route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -5,12 +5,13 @@ import {
   LoginController,
   RegisterController,
 } from "../controller/auth.controller";
+import { adminAuthMiddleware } from "../middlewares/authMiddleware";
 
 const route = Router();
 
 route.post("/user-register", RegisterController);
 route.post("/user-login", LoginController);
-route.post("/admin-register", AdminRegister);
-route.post("/admin-login", AdminLogin);
+route.post("/admin-register", adminAuthMiddleware, AdminRegister);
+route.post("/admin-login", adminAuthMiddleware, AdminLogin);
 
 export default route;
